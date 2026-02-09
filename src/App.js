abe7587a979fcda7b24c7ef43ad2be4a79a18dd8
omar ait ali <omaraitali83@gmail.com>
2026-02-09 12:49:06 +0100

    base project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles/tailwind.css"
+function App() {
+  return (
+    <div className="App">
+omar 
+    </div> 
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+    theme: {
+    extend: {
+          colors: {
+          primary: '#1A227f',
+          secondary:'#384FFE',
+          lighBlue:'#E3f2fd',
+          darkgrey:'#1F2937',
+          grayBorder:'#E5E7EB',
+          graytext:"#6b7288",
+          success:"#18B981",
+          error :'#Ef4444'
+        },
+        },
+      },
+  plugins: [],
+}
+
